7cef4c32ae5f9b88a129e86500a9891699cd41da
Addis <lumiaddis@gmail.com>
2024-01-24 16:10:21 -0300

    cambie los url de la data

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -1,12 +1,10 @@
 export default [
   {
-  const peliculasGhibli = [
-  {
     id: "nausicaa-del-valle-del-viento",
     name: "Nausicaä del Valle del Viento",
     shortDescription: "Épica historia sobre la princesa Nausicaä y su lucha por salvar su tierra de la contaminación y la guerra.",
     description: "Nausicaä del Valle del Viento es una obra maestra de Hayao Miyazaki que sigue la historia de la princesa Nausicaä en un mundo postapocalíptico. Con temas de ecología y pacifismo, la película es una emotiva aventura que aborda la relación entre la humanidad y la naturaleza.",
-    imageUrl: "Imagenes/Nausicaä-del-valle-del-viento.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Nausica%C3%A4-del-valle-del-viento.jpg",
     facts: {
       "duration": "1h 57m",
       "releaseYear": "1984",
@@ -19,7 +17,7 @@ export default [
     name: "El Castillo en el cielo",
     shortDescription: "Aventura emocionante de un joven y una niña en busca de un legendario castillo flotante.",
     description: "El Castillo en el cielo, dirigida por Hayao Miyazaki, sigue las peripecias de Pazu y Sheeta en su búsqueda del legendario Castillo en el Cielo. La película combina acción, misterio y fantasía, ofreciendo una experiencia visualmente impactante y emocionante.",
-    imageUrl: "Imagenes/El-castillo-en-el-cielo.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-castillo-en-el-cielo.jpg",
     facts: {
       "duration": "2h 4m",
       "releaseYear": "1986",
@@ -32,7 +30,7 @@ export default [
     name: "La Tumba de las Luciérnagas",
     shortDescription: "Conmovedora historia sobre dos hermanos tratando de sobrevivir en Japón durante la Segunda Guerra Mundial.",
     description: "La Tumba de las Luciérnagas es una obra maestra dirigida por Isao Takahata que narra la conmovedora historia de Seita y Setsuko, dos hermanos tratando de sobrevivir en Japón durante la Segunda Guerra Mundial. La película aborda temas de pérdida, esperanza y la devastación de la guerra.",
-    imageUrl: "Imagenes/La-tumba-de-las-luciernagas.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/La-tumba-de-las-luciernagas.jpg",
     facts: {
       "duration": "2h 4m",
       "releaseYear": "1988",
@@ -45,7 +43,7 @@ export default [
     name: "Mi Vecino Totoro",
     shortDescription: "Encantadora película de animación sobre dos hermanas que descubren criaturas mágicas en su nuevo hogar.",
     description: "Mi Vecino Totoro, dirigida por Hayao Miyazaki, es una joya del Studio Ghibli que sigue la historia de dos hermanas, Satsuke y Mei, que se mudan a un nuevo hogar y descubren a criaturas mágicas en el bosque cercano. La película captura la inocencia de la infancia y la magia de la naturaleza, ofreciendo una experiencia cinematográfica única.",
-    imageUrl: "Imagenes/Mi-vecino-Totoro.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Mi-vecino-totoro.png",
     facts: {
       "duration": "1h 26m",
       "releaseYear": "1988",
@@ -58,7 +56,7 @@ export default [
     name: "Kiki: entregas a domicilio",
     shortDescription: "Encantadora historia de una joven bruja que busca encontrar su lugar en el mundo.",
     description: "Kiki, la aprendiz de bruja, dirigida por Hayao Miyazaki, sigue la historia de Kiki, una joven bruja que emprende un viaje para encontrar su lugar en el mundo. La película destaca la autoexploración, la amistad y la superación de los desafíos. Una experiencia mágica que ha cautivado a audiencias de todas las edades.",
-    imageUrl: "Imagenes/Kiki:-entregas-a-domicilio.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Kiki-entregas-a-domicilio.webp",
     facts: {
       "duration": "1h 42m",
       "releaseYear": "1989",
@@ -71,7 +69,7 @@ export default [
     name: "Recuerdos del Ayer",
     shortDescription: "Emotiva historia que sigue a una joven que redescubre su infancia a través de recuerdos nostálgicos.",
     description: "Recuerdos del Ayer, dirigida por Isao Takahata, relata la conmovedora historia de Taeko, una mujer que, durante un viaje, redescubre su infancia a través de recuerdos nostálgicos. La película aborda temas de identidad, amor y el impacto duradero de la infancia en la vida adulta.",
-    imageUrl: "Imagenes/Recuerdos-del-ayer.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Recuerdos-del-ayer.webp",
     facts: {
       "duration": "1h 58m",
       "releaseYear": "1991",
@@ -84,7 +82,7 @@ export default [
     name: "Porco Rosso",
     shortDescription: "Aventura fascinante sobre un piloto que, después de transformarse en cerdo, lucha contra piratas aéreos en el Adriático.",
     description: "Porco Rosso, dirigida por Hayao Miyazaki, es una aventura fascinante que sigue a Marco Pagot, un piloto italiano que, tras una misteriosa transformación en cerdo antropomórfico, lucha contra piratas aéreos en el Mar Adriático. La película aborda temas de identidad, valentía y la belleza del vuelo.",
-    imageUrl: "Imagenes/Porco-Rosso.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Porco-rosso.jpg",
     facts: {
       "duration": "1h 42m",
       "releaseYear": "1992",
@@ -97,7 +95,7 @@ export default [
     name: "Puedo escuchar el mar",
     shortDescription: "Romántica historia que sigue a dos estudiantes mientras exploran la complejidad de sus emociones y relaciones.",
     description: "Puedo escuchar el mar, dirigida por Tomomi Mochizuki, es una romántica historia que sigue a dos estudiantes, Shizuku y Haru, mientras exploran la complejidad de sus emociones y relaciones. La película aborda temas de amor, autodescubrimiento y la importancia de la conexión emocional.",
-    imageUrl: "Imagenes/Puedo-escuchar-el-mar.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Puedo-escuchar-el-mar.jpg",
     facts: {
       "duration": "1h 12m",
       "releaseYear": "1993",
@@ -110,7 +108,7 @@ export default [
     name: "Pompoko",
     shortDescription: "Comedia dramática que sigue a un grupo de tanukis luchando contra la destrucción de su hábitat por parte de los humanos.",
     description: "Pompoko, dirigida por Isao Takahata, es una comedia dramática que sigue a un grupo de tanukis (criaturas mitológicas japonesas) luchando contra la destrucción de su hábitat por parte de los humanos. La película aborda temas de conservación, cambio ambiental y la relación entre la naturaleza y la sociedad.",
-    imageUrl: "Imagenes/Pompoko.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Pompoko.webp",
     facts: {
       "duration": "1h 58m",
       "releaseYear": "1994",
@@ -123,7 +121,7 @@ export default [
     name: "Susurros del Corazón",
     shortDescription: "Encantadora historia que sigue a una joven que descubre su pasión por la escritura y la creatividad.",
     description: "Susurros del Corazón, dirigida por Yoshifumi Kondō, es una encantadora historia que sigue a Shizuku, una joven que descubre su pasión por la escritura y la creatividad mientras se embarca en un viaje de autodescubrimiento. La película celebra la importancia de seguir los sueños y la búsqueda de la propia identidad.",
-    imageUrl: "Imagenes/Susurros-del-Corazon.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Susurros-del-coraz%C3%B3n.webp",
     facts: {
       "duration": "1h 46m",
       "releaseYear": "1995",
@@ -136,7 +134,7 @@ export default [
     name: "La princesa Mononoke",
     shortDescription: "Épica aventura que sigue la lucha entre los dioses del bosque y los humanos en busca de recursos.",
     description: "La princesa Mononoke, dirigida por Hayao Miyazaki, es una épica aventura que sigue la lucha entre los dioses del bosque y los humanos en busca de recursos. La película explora temas de coexistencia, equilibrio ambiental y la complejidad de las relaciones entre la naturaleza y la humanidad.",
-    imageUrl: "Imagenes/La-princesa-Mononoke.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/La-princesa-Mononoke.webp",
     facts: {
       "duration": "2h 13m",
       "releaseYear": "1997",
@@ -149,7 +147,7 @@ export default [
     name: "Mis vecinos Los Yamada",
     shortDescription: "Comedia familiar que presenta la vida cotidiana de una peculiar familia japonesa.",
     description: "Mis vecinos Los Yamada, dirigida por Isao Takahata, es una comedia familiar que presenta la vida cotidiana de una peculiar familia japonesa. La película utiliza un estilo de animación único para capturar los momentos divertidos y conmovedores de la vida diaria, mostrando la belleza en lo ordinario.",
-    imageUrl: "Imagenes/Mis-Vecinos-Los-Yamada.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Mis-vecinos-los-Yamada.jpg",
     facts: {
       "duration": "1h 44m",
       "releaseYear": "1999",
@@ -162,7 +160,7 @@ export default [
     name: "El viaje de Chihiro",
     shortDescription: "Mágica aventura que sigue a una niña atrapada en un mundo misterioso y su viaje para salvar a sus padres.",
     description: "El viaje de Chihiro, dirigida por Hayao Miyazaki, es una mágica aventura que sigue a Chihiro, una niña que queda atrapada en un mundo misterioso lleno de seres fantásticos. La película explora temas de valentía, amistad y autodescubrimiento mientras Chihiro lucha por salvar a sus padres y encontrar su camino de regreso a casa.",
-    imageUrl: "Imagenes/El-viaje-de-Chihiro.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-viaje-de-Chihiro.png",
     facts: {
       "duration": "2h 3m",
       "releaseYear": "2001",
@@ -175,7 +173,7 @@ export default [
     name: "Haru en el reino de los gatos",
     shortDescription: "Encantadora historia sobre una joven que se encuentra en un mundo de gatos y descubre su valentía interior.",
     description: "Haru en el reino de los gatos, dirigida por Hiroyuki Morita, es una encantadora historia que sigue a Haru, una joven que, después de salvar a un gato, se encuentra en un mundo habitado por gatos y descubre su valentía interior. La película mezcla elementos de fantasía y comedia para contar una historia única sobre el crecimiento personal y la autoaceptación.",
-    imageUrl: "Imagenes/Haru-en-el-reino-de-los-Gatos.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/haru-en-el-reino-de-los-gatos.jpg",
     facts: {
       "duration": "1h 15m",
       "releaseYear": "2002",
@@ -188,7 +186,7 @@ id: "el-castillo-ambulante",
     name: "El Castillo Ambulante",
     shortDescription: "Aventura mágica que sigue a una joven que, tras ser maldecida, busca la ayuda de un mago en un castillo ambulante.",
     description: "El Castillo Ambulante, dirigida por Hayao Miyazaki, es una aventura mágica que sigue a Sophie, una joven que, tras ser maldecida y transformada en una anciana, busca la ayuda de un mago en un castillo ambulante. La película aborda temas de autoaceptación, amor y la magia que se encuentra en lo inesperado.",
-    imageUrl: "Imagenes/El-castillo-ambulante.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-castillo-ambulante.jpg",
     facts: {
       "duration": "1h 59m",
       "EreleaseYear": "2004",
@@ -201,7 +199,7 @@ id: "el-castillo-ambulante",
     name: "Cuentos de Terramar",
     shortDescription: "Fantástica historia que sigue a un joven príncipe en su viaje para restaurar el equilibrio en el mundo.",
     description: "Cuentos de Terramar, dirigida por Gorō Miyazaki, es una fantástica historia que sigue a Arren, un joven príncipe, en su viaje para restaurar el equilibrio en el mundo. La película, basada en la serie de libros de Ursula K. Le Guin, aborda temas de autodescubrimiento, responsabilidad y la conexión entre la humanidad y la naturaleza.",
-    imageUrl: "Imagenes/Cuentos-de-Terramar.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Cuentos-de-Terramar.jpg",
     facts: {
       "duration": "1h 55m",
       "releaseYear": "2006",
@@ -214,7 +212,7 @@ id: "el-castillo-ambulante",
     name: "Ponyo y el secreto de la sirenita",
     shortDescription: "Encantadora historia sobre la amistad entre un niño y un pez dorado que desea convertirse en humana.",
     description: "Ponyo en el acantilado, dirigida por Hayao Miyazaki, es una encantadora historia que sigue la amistad entre Sosuke, un niño, y Ponyo, un pez dorado con el deseo de convertirse en humana. La película celebra la imaginación, la conexión con la naturaleza y la pureza de la infancia.",
-    imageUrl: "Imagenes/Ponyo.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Ponyo.jpg",
     facts: {
       "duration": "1h 40m",
       "releaseYear": "2008",
@@ -227,7 +225,7 @@ id: "el-castillo-ambulante",
     name: "Arrietty y el Mundo de los Diminutos",
     shortDescription: "Adorable historia que sigue a una familia de diminutos seres que viven bajo el suelo de una casa humana.",
     description: "Arrietty y el Mundo de los Diminutos, dirigida por Hiromasa Yonebayashi, es una adorable historia que sigue a una familia de diminutos seres, los Diminutos, que viven bajo el suelo de una casa humana. La película explora la conexión entre los mundos de los humanos y los diminutos, destacando la importancia de la empatía y la comprensión mutua.",
-    imageUrl: "Imagenes/Arrietty-y-el-mundo-de-los-diminutos.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Arrietty-y-el-mundo-de-los-diminutos.jpg",
     facts: {
       "duration": "1h 34m",
       "releaseYear": "2010",
@@ -240,7 +238,7 @@ id: "el-castillo-ambulante",
     name: "La colina de las Amapolas",
     shortDescription: "Conmovedora historia que sigue a dos jóvenes que luchan por proteger su hogar y superar la adversidad.",
     description: "La colina de las amapolas, dirigida por Goro Miyazaki, es una conmovedora historia que sigue a Umi y Shun, dos jóvenes que luchan por proteger su hogar y superar la adversidad mientras descubren secretos sobre su pasado. La película aborda temas de amor, identidad y la importancia de la memoria.",
-    imageUrl: "Imagenes/La-colina-de-las-Amapolas.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/La-Colina-de-las-Amapolas.jpg",
     facts: {
       "duration": "1h 31m",
       "releaseYear": "2011",
@@ -253,7 +251,7 @@ id: "el-castillo-ambulante",
     name: "El viento se levanta",
     shortDescription: "Drama animado que sigue la vida de Jiro Horikoshi, el diseñador de aviones japonés, durante la Segunda Guerra Mundial.",
     description: "El viento se levanta, dirigida por Hayao Miyazaki, es un drama animado que sigue la vida de Jiro Horikoshi, el diseñador de aviones japonés, durante la Segunda Guerra Mundial. La película explora temas de pasión, creatividad y las complejidades morales en tiempos de conflicto.",
-    imageUrl: "Imagenes/El-viento-se-levanta.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-viento-se-levanta.jpg",
     facts: {
       "duration": "2h 6m",
       "releaseYear": "2013",
@@ -266,7 +264,7 @@ id: "el-castillo-ambulante",
     name: "El cuento de la princesa Kaguya",
     shortDescription: "Emotiva historia que sigue el viaje de Kaguya-hime, una princesa misteriosa, en busca de la felicidad.",
     description: "El cuento de la princesa Kaguya, dirigida por Isao Takahata, es una emotiva historia que sigue el viaje de Kaguya-hime, una princesa misteriosa enviada desde la Luna, en busca de la verdadera felicidad. La película utiliza un estilo artístico único y aborda temas de identidad, amor y la efímera naturaleza de la vida.",
-    imageUrl: "Imagenes/El-cuento-de-la-princesa-Kaguya.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-cuento-de-la-princesa-Kaguya.jpg",
     facts: {
       "duration": "2h 17m",
       "releaseYear": "2013",
@@ -279,7 +277,7 @@ id: "el-castillo-ambulante",
     name: "El recuerdo de Marnie",
     shortDescription: "Emotiva historia que sigue a Anna, una joven solitaria que descubre la amistad y los secretos de una misteriosa chica.",
     description: "El recuerdo de Marnie, dirigida por Hiromasa Yonebayashi, es una emotiva historia que sigue a Anna, una joven solitaria que descubre la amistad y los secretos de una misteriosa chica llamada Marnie. La película aborda temas de soledad, aceptación y la conexión especial que se forma entre almas afines.",
-    imageUrl: "Imagenes/El-recuerdo-de-Marnie.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-recuerdo-de-Marnie.jpg",
     facts: {
       "duration": "1h 43m",
       "releaseYear": "2014",
@@ -292,7 +290,7 @@ id: "el-castillo-ambulante",
     name: "Earwig y la bruja",
     shortDescription: "Aventura animada que sigue a Earwig, una niña astuta, mientras se enfrenta a una bruja y descubre su propio poder.",
     description: "Earwig y la bruja, dirigida por Goro Miyazaki, es una aventura animada que sigue a Earwig, una niña astuta, mientras se enfrenta a una bruja y descubre su propio poder. La película, basada en la novela de Diana Wynne Jones, aborda temas de ingenio, valentía y el poder de la autodeterminación.",
-    imageUrl: "Imagenes/Earwig-y-la-bruja.jpg",
+    imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/Earwig-y-la-bruja.webp",
     facts: {
       "duration": "1h 22m",
       "releaseYear": "2020",
@@ -305,7 +303,7 @@ id: "el-castillo-ambulante",
   name: "El niño y la garza",
   shortDescription: "Mahito, un joven de 12 años, lucha por asentarse en una nueva ciudad tras la muerte de su madre.",
   description: "El niño y la garza es una película de animación de 2023 escrita y dirigida por Hayao Miyazaki, con producción de Studio Ghibli. Basada en la novela homónima de 1937 de Yoshino Genzaburō, la película presenta una historia original no relacionada con la novela. Miyazaki la considera su película más simbólica y personal, explorando su propia vida. Se estrenó el 14 de julio de 2023 en Japón y ha sido aclamada por la crítica.",
-  imageUrl: "Imagenes/El-niño-y-la-garza.jpg",
+  imageUrl: "https://raw.githubusercontent.com/Addis-cx/DEV013-dataverse/main/src/Imagenes/El-chico-y-la-garza.jpg",
   facts: {
     "duration": "2h 4m",
     "releaseYear": "2023",
@@ -313,4 +311,4 @@ id: "el-castillo-ambulante",
     "boxOfficeRevenue": "$10 millones en taquilla."
    }
  },
-]}]; 
+];
