57cb1638ab5f5617f27fcea12e34683cca279dbd
Addis <lumiaddis@gmail.com>
2024-01-25 01:25:02 -0300

    Creamos la funcion para renderizar la data

--- src/view.js
+++ src/view.js
@@ -6,7 +6,14 @@ export const renderItems = (data) => {
     const card = document.createElement("li");
     card.innerHTML+=`
     <img src="${film.imageUrl}" alt="Afiche de la película ${film.name}">
-    <dt>Nombre: </dt><dd itemprop="name">${film.name}</dd>
+    <ul itemscope itemtype="nausicaa-del-valle-del-viento"></ul>
+    <li>Nombre: </li><li itemprop="name">${film.name}</li>
+    <li>Descripción breve: </li><li itemprop="shortDescription">${film.shortDescription}</li>
+    <li>Descripción: </li><li itemprop="description">${film.description}</li>
+    <li>Duración: </li><li itemprop="duration">${film.duration}</li>
+    <li>Estreno: </li><li itemprop="eleaseYear"li>${film.eleaseYear}</li>
+    <li>Género: </li><li itemprop="genres">${film.genres}</li>
+    <li>Recaudación: </li><li itemprop="boxOfficeRevenue">${film.boxOfficeRevenue}</li>
     `
     list.appendChild(card);
   });
