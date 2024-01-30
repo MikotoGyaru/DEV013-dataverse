d0b95a2e6eb61fd6b2afdd90f27df694b65ee429
MikotoGyaru <catalinealarcon@gmail.com>
2024-01-30 09:36:38 -0300

    Agregué algunas funciones en main.js para darle funcionalidad a los filtro

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-const movieCards = document.querySelector("#root");
-movieCards.appendChild(renderItems(data));
+document.addEventListener("DOMContentLoaded", function () {
+    const movieCards = document.querySelector("#root");
+    movieCards.appendChild(renderItems(data));
+  
+    const cleanerButton = document.querySelector(".cleanerButton");
+    cleanerButton.addEventListener("click", function() {
+   
+      const filteredAndSortedData = dataFunction.sortBy(data, 'name', 'asc');
+      movieCards.innerHTML = '';
+      movieCards.appendChild(renderItems(filteredAndSortedData));
+    });
+  
+    const filter = document.getElementById("filter");
+    const order = document.getElementById("order");
 
-const cleanerButton = document.querySelector(".cleanerButton");
-cleanerButton.addEventListener("click", function() { 
-    
-})
-
-console.log(example, renderItems(data), data);
-renderItems(data)
+  });
