601148f2fa04e97a24f318b63472258d790a8b1b
Addis <lumiaddis@gmail.com>
2024-02-05 14:06:29 -0300

    cree divs para poder darles estilos mas detallados a los botonos y el input

--- src/view.js
+++ src/view.js
@@ -1,21 +1,25 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
   const list = document.createElement("ul");
-  data.forEach(film => {
+  data.forEach((film) => {
     const card = document.createElement("li");
-    card.innerHTML+=`
+    card.innerHTML += `
+    <div class="card"> 
+    <div class="visual"> 
     <img src="${film.imageUrl}" alt="Afiche de la película ${film.name}">
     <ul itemscope itemtype="nausicaa-del-valle-del-viento">
-    <li itemtype="name">Nombre: ${film.name}</li>
+    <li itemtype="name">${film.name}</li>
+    <li itemtype"genres">Género: ${film.facts["genders"]}</li>
+    </div>
+    <div class="noVisual"> 
     <li itemtype"shortDescription">Sinopsis: ${film.shortDescription}</li>
     <li itemtype="description">Descripción: ${film.description}</li>
     <li itemtype="duration">Duración: ${film.facts["duration"]}</li>
     <li itemtype="releaseYear">Estreno: ${film.facts["releaseYear"]}</li>
-    <li itemtype"genres">Género: ${film.facts["genders"]}</li>
     <li itemtype="boxOfficeRevenue">Recaudación: ${film.facts["boxOfficeRevenue"]}</li>
+    </div>
     </ul>
-    `
+    </div>
+    `;
     list.appendChild(card);
   });
   return list;
